abd7ea71903f3b266b2a9628ac663601b99c4e0a
Kong <newlight77@gmail.com>
2025-01-18 15:22:10 +0100

    feature(search video): update followings list

--- src/components/data/followings.ts
+++ src/components/data/followings.ts
@@ -1,14 +1,13 @@
 const FOLLOWINGS = [
     { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'VitaAnimation' },
     { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'MM_8888' },
-    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'DonghuaStream' },
-    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'All Keys Donghua' },
+    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'donghuaguoman' },
+    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'allkeys' },
     { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'ARTFart' },
-    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'chikianimation.net' },
-    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'Donghuafilm.com' },
-    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'Vita Animation Groups' },
-    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'Xiao bhai' },
-    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'Anime Zone' },
+    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'abrarsikandar452' },
+    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'DonghuafilmOfficial' },
+    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'xiaobhai7' },
+    { uid: crypto.randomUUID().toString(),  order: 1   ,  owner: 'animezone0603' },
 ]
 
 export default FOLLOWINGS;
