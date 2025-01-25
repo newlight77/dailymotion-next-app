2e1cb6427e4b99ee3d6b91f68ed5fd701dbcc83c
Kong <newlight77@gmail.com>
2025-01-25 14:16:45 +0100

    ui: change background of navbar

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -47,9 +47,9 @@ const VideoPage: React.FC = () => {
           ))}
         </ul>
 
-        <div className='sm:showd'>
+        <div className='sm:show'>
           {nav && (
-            <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500" >
+            <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-secondary to-secondary text-primary" >
               {links.map(({ id, label, route }) => (
                 <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl" >
                   <Link href={route} onClick={toggleShowNav} >{label}</Link>
