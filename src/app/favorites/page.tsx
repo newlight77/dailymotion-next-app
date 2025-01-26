e4b5d9b2e719052842d64f503a555abf419e1e77
Kong <newlight77@gmail.com>
2025-01-26 17:44:14 +0100

    ui: adjust width on page for list of items

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -17,7 +17,7 @@ const FavoritesPage: React.FC = () => {
         <div className='w-full'>
             <h2 className='title text-3xl p-1 md:p-4 capitalize'>my favorites videos</h2>
 
-            <div className="pb-4 pt-4 md:absolute md:z-100 md:p-2 md:m-1 max-w-screen-sm">
+            <div className="pb-4 pt-4 md:absolute md:z-100 md:p-2 md:m-1 sm:max-w-screen-sm md:max-w-screen-xl">
                 <Favorites onSelected={handleSelectFavorite} />
             </div>
 
--- src/app/followings/page.tsx
+++ src/app/followings/page.tsx
@@ -9,7 +9,7 @@ const FollowingsPage: React.FC = () => {
         <div className='w-full'>
             <h2 className='title text-3xl p-1 md:p-4 capitalize'>my followings</h2>
 
-            <div className='pb-4 pt-4 md:absolute md:z-100 md:p-2 md:m-1 max-w-screen-sm'>
+            <div className='pb-4 pt-4 md:absolute md:z-100 md:p-2 md:m-1 sm:max-w-screen-sm md:max-w-screen-xl'>
                 <Followings />
             </div>
         </div>
