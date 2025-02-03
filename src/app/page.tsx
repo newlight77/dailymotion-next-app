a0b0c5647a7245716853ad4c984c58183b0ef53d
Kong <newlight77@gmail.com>
2025-02-03 21:20:31 +0100

    ui: remove collapsable favorite in search page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 "use client"
 
 import React, { Suspense } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import Search from '@/bounded-contexts/video-search-context/view/search/Search';
-import Collapsable from '@/components/molecules/Collapsable';
-import Favorites from '@/bounded-contexts/video-search-context/view/favorites/Favorites';
-import { FavoriteType } from '@/bounded-contexts/video-search-context/domain/model/anime';
-
 
 const SearchWithParams: React.FC = () => {
     const searchParams = useSearchParams()
@@ -21,27 +17,10 @@ const SearchWithParams: React.FC = () => {
 
 
 const HomePage: React.FC = () => {
-    const [hideShow, toggleHideShow] = React.useState<boolean>();
-    const router = useRouter()
-
-    const handleSelectFavorite = (favorite: FavoriteType) => {
-        toggleHideShow(!hideShow);
-        const title = encodeURIComponent(`${favorite.title} ${favorite.originalTitle ? favorite.lastEpisode : ''}`)
-        router.push(`/?keywords=${title}`);
-    };
-
 
     return (
         <div className="w-full">
             <h2 className='title text-3xl p-1 md:p-4 capitalize'>search videos</h2>
-
-            <Collapsable
-                hideShow={hideShow}
-                className="pb-4 pt-4 right-0 md:absolute md:z-100 md:p-2 md:m-1"
-                title={'My favorites'}
-                collapsedLabel={'show my favorites'}>
-                <Favorites className='max-w-screen-sm' onSelected={handleSelectFavorite} />
-            </Collapsable>
 
             <Suspense>
                 <SearchWithParams></SearchWithParams>
