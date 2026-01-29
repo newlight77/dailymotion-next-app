3697bc48daa8b7fa1220e7557ab9ccc08e53f9f0
Kong <newlight77@gmail.com>
2026-01-29 09:33:25 +0100

    filtering anime lists per ratings

--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,7 +1,7 @@
-export const version = "2026.01.28-5d1d86d";
-export const prefix = "2026.01.28";
-export const suffix = "5d1d86d";
-export const commitDate = "2026-01-28 18:42:36 +0100";
+export const version = "2026.01.29-4d8727b";
+export const prefix = "2026.01.29";
+export const suffix = "4d8727b";
+export const commitDate = "2026-01-29 09:11:12 +0100";
 
 const versionModule = { version, prefix, suffix, commitDate };
 export default versionModule;
